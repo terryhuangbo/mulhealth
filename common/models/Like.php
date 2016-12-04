64344642e7ed5fce86b9004c638d3ec45754d254
bhuang <3121045133@qq.com>
2016-12-04 09:21:03 +0800

    【开发】增加点赞表

---
+++ common/models/Like.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use common\base\BaseModel;
+
+/**
+ * This is the model class for table "{{%like}}".
+ *
+ * @property integer $id
+ * @property integer $uid
+ * @property integer $cid
+ * @property integer $create_at
+ */
+class Like extends BaseModel
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%like}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['uid', 'cid', 'create_at'], 'integer']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => '点赞ID',
+            'uid' => '用户ID',
+            'cid' => '评论ID',
+            'create_at' => '创建时间',
+        ];
+    }
+
+    /**
+     * 关联用户表
+     * @return \yii\db\ActiveQuery
+     **/
+    public function getUser() {
+        return $this->hasOne(User::className(), ['uid' => 'uid']);
+    }
+
+    /**
+     * 关联评论表
+     * @return \yii\db\ActiveQuery
+     **/
+    public function getComment() {
+        return $this->hasOne(Comment::className(), ['cid' => 'id']);
+    }
+
+    
+
+
+}
